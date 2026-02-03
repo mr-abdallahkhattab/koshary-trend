936afbaf89f60c40b530a028bb02b1625f4ae388
Abdallah Khattab <iiimr.abdullah@gmail.com>
2026-02-03 19:59:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "koshary-trend-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/index.js",
+  "/js/bootstrap.bundle.min.js",
+  "/css/bootstrap.min.css",
+  "/images/icon-192.png",
+  "/images/icon-512.png",
+];
+
+// 1. Install Service Worker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Activate Service Worker
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
